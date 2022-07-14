a7f24885610806add7c7c0c05231e6168fb9b320
Himanshi Jain <107564621+himanshijainn@users.noreply.github.com>
2022-07-14 20:39:34 +0530

    Initial commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='container' Header>
+            <img src='https://thumbs.dreamstime.com/b/dictionary-dictionary-study-translate-bilingual-english-background-book-117950458.jpg'
+             height={"400px"} width={"400px "}  > 
+            </img> </div>
+    )
+}
+
+export default Header
